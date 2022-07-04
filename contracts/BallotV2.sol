b633134a92630379521a423ea8a48514f6be75fd
Thanh Pham <ptt0412@gmail.com>
2022-07-04 21:25:29 +0700

    Add validation for Ballot with modifier

---
+++ contracts/BallotV2.sol
@@ -0,0 +1,78 @@
+pragma solidity ^0.4.0;
+
+contract Ballot {
+    struct Voter {
+        uint weight;
+        bool voted;
+        uint8 vote;
+    }
+
+    struct Proposal {
+        uint voteCount;
+    }
+
+    enum Stage {Init, Reg, Vote, Done}
+    Stage public stage = Stage.Init;
+
+    address chairperson;
+    mapping (address => Voter) voters;
+
+    uint startTime;
+    Proposal[] proposals;
+
+    modifier validStage(Stage reqStage) {
+        require (stage == reqStage);
+        _;
+    }
+
+    event votingCompleted();
+
+
+    function Ballot(uint8 _numProposals) public {
+        chairperson = msg.sender;
+        voters[chairperson].weight = 2;
+        proposals.length = _numProposals;
+        stage = Stage.Reg;
+        startTime = now;
+    }
+
+    function register(address toVoter) public validStage(Stage.Reg) {
+        //if (stage != Stage.Reg) return;
+        if (msg.sender != chairperson || voters[toVoter].voted) {
+            return;
+        }
+        voters[toVoter].weight = 1;
+        voters[toVoter].voted = false;
+        if (now > (startTime + 10 seconds)) {
+            stage = Stage.Vote;
+            startTime = now;
+        }
+    }
+
+    function vote(uint8 toProposal) public validStage(Stage.Vote) {
+        //if (stage != Stage.Vote) return;
+        Voter storage sender = voters[msg.sender];
+        if (sender.voted || toProposal >= proposals.length) {
+            return;
+        }
+        sender.voted = true;
+        sender.vote = toProposal;
+        proposals[toProposal].voteCount += sender.weight;
+        if (now > (startTime + 10 seconds)) {
+            stage = Stage.Done;
+            startTime = now;
+            votingCompleted();
+        }
+    } 
+
+    function winningProposal() public validStage(Stage.Done) constant returns (uint8 _winningProposal) {
+        //if (stage != Stage.Done) return;
+        uint256 winningVoteCount = 0;
+        for (uint8 prop = 0; prop < proposals.length; prop++) {
+            if (proposals[prop].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[prop].voteCount;
+                _winningProposal = prop;
+            }
+        }
+    }
+}
